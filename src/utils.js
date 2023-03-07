c508bf8bdce9793ade44f25f564845705573d2ab
Orest Bida <dev@orestbida.com>
2023-03-08 00:54:30 +0100

    Code refactoring

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,16 @@
 /**
- * @param {string} selector
- * @returns {NodeListOf<HTMLAnchorElement>}
- */
-export const getLinks = () => document.querySelectorAll('a');
-
-/**
  * @param {HTMLAnchorElement} link
  */
 export const validLink = (link) => link.host === location.host && link.pathname !== location.pathname;
 
 /**
+ * @param {NodeListOf<HTMLAnchorElement>} [links]
+ */
+export const getValidLinks = (links) => [...(links || document.querySelectorAll('a'))].filter(validLink);
+
+/**
  * @param {HTMLElement} el
- * @param {keyof WindowEventMap} type
+ * @param {(keyof WindowEventMap)[]} type
  * @param {(ev: Event) => {}} fn
  */
-export const addEvent = (el, type, fn) => el.addEventListener(type, fn);
+export const addEvents = (el, type, fn) => type.forEach(type => el.addEventListener(type, fn));
